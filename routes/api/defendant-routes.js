123565ca568e5c23f4a305653bf386672fe49eac
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-11 12:24:39 -0600

    finished work on defendant api routes

---
+++ routes/api/defendant-routes.js
@@ -0,0 +1,97 @@
+const router = require('express').Router();
+const { Defendant } = require('../../models');
+
+// routes built in order to follow CRUD (create, read, update, delete) model.
+
+//create a new user.
+router.post('/', (req, res) => {
+    Defendant.create({
+        parcel_id: req.body.parcel_id,
+        def_name: req.body.def_name,
+        co_def: req.body.co_def,
+        amount: req.body.amount,
+        sale_date: req.body.sale_date,
+        owner_mail_add: req.body.owner_mail_add,
+        city_state: req.body.city_state
+
+    })
+    .then(defendantData => {
+        res.json(defendantData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// get all users
+router.get('/', (req, res) => {
+    Defendant.findAll({})
+    .then(defendantData => res.json(defendantData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+//get specific user by id
+router.get('/:id', (req, res) => {
+    Defendant.findOne({
+        where: {
+            id: req.params.id
+        },
+    })
+    .then(defendantData => {
+        if(!defendantData) {
+            res.status(404).json({ message: ''})
+            return;
+        }
+        res.json(defendantData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+// update user info
+router.put('/:id', (req,res) => {
+    Defendant.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(defendantData => {
+        if(!defendantData) {
+            res.status(404).json({ message: ''});
+        }
+        res.json(defendantData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//delete a user
+router.delete('/:id', (req, res) => {
+    Defendant.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(defendantData => {
+        if(!defendantData) {
+            res.status(404).json({ message: ''})
+        }
+        res.json(defendantData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
+
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 userRoutes = require('./user-routes');
+defendantRoutes = require('./defendant-routes');
 
 router.use('/user', userRoutes);
+router.use('/defendant', defendantRoutes);
 
 module.exports = router;
