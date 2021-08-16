ae871893ff98771d06fc01ccfc768f443e1ca355
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-16 16:31:19 -0600

    added jawsdb compatability code

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,23 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_name, process.env.DB_user, process.env.DB_pass, {
-    host: 'localhost',
-    dialect: 'mysql',
-    post: 1337
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    
+    sequelize = new Sequelize(
+        process.env.DB_name, 
+        process.env.DB_user, 
+        process.env.DB_pass, 
+        {
+        host: 'localhost',
+        dialect: 'mysql',
+        post: 1337
+        }
+    );
+}
+
 
 module.exports = sequelize;
