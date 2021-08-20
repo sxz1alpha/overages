06cd9f9ce3ab972b92b4cbfa510561d97814b9d4
UCDT <adrian.j.ulibarri@gmail.com>
2021-08-19 19:30:06 -0600

    finished modal functionality

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,22 +1,40 @@
 
+
 $('#hotlist-ul').on('click', 'button', async function(event) {
     event.preventDefault();
 
     let id = event.target.id
     
-    const response = await fetch(`/api/defendant/${id}`, {
+    await fetch(`/api/defendant/${id}`, {
         method: 'GET',   
     })
-    
+    .then(response => response.json())
+    .then((defendant) => {
+        if(defendant) {
 
-    if(response.ok) {
-        // let data = response.json();
-        console.log(response);
-        // $('#myModal').modal('show');
+            $('#modal-parcel-id').replaceWith(defendant.parcel_id)
+            $('#modal-def-name').replaceWith(defendant.def_name)
+            $('#modal-co-def').replaceWith(defendant.co_def_name)
+            $('#modal-amount').replaceWith(defendant.amount)
+            $('#modal-sale-date').replaceWith(defendant.sale_date)
+            $('#modal-address').replaceWith(defendant.owner_mail_add)
+            $('#modal-city-state').replaceWith(defendant.city_state)
 
-    } else {
-        alert(response.statusText)
-    }
+            $('#myModal').modal('show');
+
+        } else {
+            alert("defendant not found");
+        }
+    })
+
+    // if(response.ok) {
+    //     let data = response.json()
+    //     console.log(data);
+    //     let defendant = 'hahahahahahahaha test' //TODO: find parent defendant object in the dom
+
+
+    // } else {
+    // }
 
 });
 
